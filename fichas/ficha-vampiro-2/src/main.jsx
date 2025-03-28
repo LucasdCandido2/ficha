62712b7379a0d11d83f9ee90947db206e22424b5
LucasdCandido2 <lucasdcandido1994@gmail.com>
2025-03-28 14:59:37 -0300

    atualizando fichas

--- fichas/ficha-vampiro-2/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { FichaProvider } from './context/FichaContext';
-import './index.css'; //Caso queira adicionar estilos globais
-
